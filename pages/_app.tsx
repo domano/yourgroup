180bca3f8f320a86789356018b5285fdfdba63ae
Dino Omanovic <dino@gomano.de>
2022-01-08 21:26:26 +0100

    Try to get tailwind working on vercel prod...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,13 +10,9 @@ import Layout from "../components/templates/layout";
 
 function MyApp({Component, pageProps}) {
     return <RecoilRoot>
-        <html className="h-full bg-gray-100">
-        <body className="h-full">
         <Layout>
             <Component {...pageProps} />
         </Layout>
-        </body>
-        </html>
     </RecoilRoot>
 }
 
