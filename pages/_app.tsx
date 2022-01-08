1916029c847b6f94f50b569610430cc5c5969145
Dino Omanovic <dino@gomano.de>
2022-01-08 16:12:58 +0100

    Initial bootstrap of next with tailwind & recoiljs

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import '../styles/globals.css'
+import {
+    RecoilRoot,
+    atom,
+    selector,
+    useRecoilState,
+    useRecoilValue,
+} from 'recoil';
+import Layout from "../components/layout";
+
+function MyApp({Component, pageProps}) {
+    return <RecoilRoot>
+        <html className="h-full bg-gray-100">
+        <body className="h-full">
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+        </body>
+        </html>
+    </RecoilRoot>
+}
+
+export default MyApp
