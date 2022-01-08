cd70333bc31d9ea60626d477dea878c03eff0243
Dino Omanovic <dino@gomano.de>
2022-01-08 17:57:31 +0100

    Revert "Try to gett tailwind working on vercel prod..."
    This reverts commit 4fbdd07466311bcfd97112720b34eb14d8fa3eca.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,22 @@
 import '../styles/globals.css'
 import {
     RecoilRoot,
+    atom,
+    selector,
+    useRecoilState,
+    useRecoilValue,
 } from 'recoil';
 import Layout from "../components/templates/layout";
 
 function MyApp({Component, pageProps}) {
     return <RecoilRoot>
+        <html className="h-full bg-gray-100">
+        <body className="h-full">
         <Layout>
             <Component {...pageProps} />
         </Layout>
+        </body>
+        </html>
     </RecoilRoot>
 }
 
